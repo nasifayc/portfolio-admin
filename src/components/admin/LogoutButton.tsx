98d932689bfb2a8834a4ecaeb041f7c8c75501af
nasifayc <nasifayc11@gmail.com>
2025-04-24 15:25:07 +0300

    fix: minor bug fixes

--- src/components/admin/LogoutButton.tsx
+++ src/components/admin/LogoutButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { Button } from "../ui/button";
 import { Loader2 } from "lucide-react";
 
@@ -9,43 +9,43 @@ import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
 function LogoutButton() {
-  const [loading, setLoading] = useState(false);
+  const [isPending, startTransition] = useTransition();
   const router = useRouter();
 
-  const handleLogout = async () => {
-    setLoading(true);
-    const { errorMessage } = await logoutAction();
+  const handleLogout = () => {
+    startTransition(async () => {
+      const { errorMessage } = await logoutAction();
 
-    if (!errorMessage) {
-      toast.success("Successfully logged out!", {
-        description: "You have been successfully logged out.",
-        style: {
-          backgroundColor: "#4CAF50",
-          color: "white",
-          border: "1px solid #388E3C",
-        },
-      });
-      router.push("/");
-    } else {
-      toast.error("Failed to log out!", {
-        description: errorMessage,
-        style: {
-          backgroundColor: "#F44336",
-          color: "white",
-          border: "1px solid #D32F2F",
-        },
-      });
-    }
-    setLoading(false);
+      if (!errorMessage) {
+        toast.success("Successfully logged out!", {
+          description: "You have been successfully logged out.",
+          style: {
+            backgroundColor: "#4CAF50",
+            color: "white",
+            border: "1px solid #388E3C",
+          },
+        });
+        router.push("/");
+      } else {
+        toast.error("Failed to log out!", {
+          description: errorMessage,
+          style: {
+            backgroundColor: "#F44336",
+            color: "white",
+            border: "1px solid #D32F2F",
+          },
+        });
+      }
+    });
   };
   return (
     <Button
-      className="flex w-24 items-center justify-center sm:block"
-      disabled={loading}
+      className="flex w-24 items-center justify-center"
+      disabled={isPending}
       variant="outline"
       onClick={handleLogout}
     >
-      {loading ? <Loader2 className="animate-spin" /> : "Logout"}
+      {isPending ? <Loader2 className="animate-spin" /> : "Logout"}
     </Button>
   );
 }
